498a4384525a4d6debc50e5b42e447be29d65eed
BlackBright19 <BlackBright19@duck.com>
2026-01-03 20:38:49 +0100

    Revert "test muteks"
    This reverts commit 5a5d6e1a1891e21819768f5cc08682ace129aa96.

--- watek_komunikacyjny.c
+++ watek_komunikacyjny.c
@@ -2,8 +2,6 @@
 #include "watek_komunikacyjny.h"
 #include "util.h"
 
-/* ... nagłówki ... */
-
 void *startKomWatek(void *ptr)
 {
     MPI_Status status;
@@ -12,7 +10,7 @@ void *startKomWatek(void *ptr)
     while ( stan!=InFinish ) {
         MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-        // Aktualizacja zegara Lamporta
+        // Aktualizacja zegara Lamporta (Max + 1)
         pthread_mutex_lock(&clockMut);
         if (pakiet.ts > lamport_clock) lamport_clock = pakiet.ts;
         lamport_clock++;
@@ -20,22 +18,20 @@ void *startKomWatek(void *ptr)
 
         switch ( status.MPI_TAG ) {
             case REQUEST:
-                // SEKCJA KRYTYCZNA: Zapis do tablic
-                pthread_mutex_lock(&tablicaMut);
+                // debug("Otrzymałem REQ od %d...", pakiet.src);
+
+                // Aktualizujemy tablice wiedzy o innych
                 tablica_zadan[pakiet.src] = pakiet.ts;
                 tablica_zasobow[pakiet.src] = pakiet.resource_id;
-                pthread_mutex_unlock(&tablicaMut);
-                // KONIEC SEKCJI
 
+                // Odsyłamy TYLKO JEDEN pakiet ACK
                 sendPacket( 0, status.MPI_SOURCE, ACK );
                 break;
 
             case RELEASE:
-                // SEKCJA KRYTYCZNA: Zapis do tablic
-                pthread_mutex_lock(&tablicaMut);
+                // debug("Dostałem RELEASE od %d", pakiet.src);
+                // Ktoś wyszedł, więc czyścimy jego wpis w tablicy
                 tablica_zadan[pakiet.src] = -1;
-                pthread_mutex_unlock(&tablicaMut);
-                // KONIEC SEKCJI
                 break;
 
             case ACK:
